e7d0799b726d06fa74aed2ce1d57024277616eb7
Purushotam Soudagar <purushotamsoudagar100@gmail.com>
2025-07-26 12:27:40 +0530

    Completed Design Type Radio button

--- Expense Tracker/Program.cs
+++ Expense Tracker/Program.cs
@@ -9,6 +9,9 @@ builder.Services.AddControllersWithViews();
 //DI
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnections")));
+
+//Register Licence
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1JEaF5cXmRCdkx0WmFZfVtgdl9EYFZSQGYuP1ZhSXxWdkJhX39ccnRWQmNcUUJ9XEI=");
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -18,6 +21,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 
 app.UseHttpsRedirection();
 app.UseRouting();
