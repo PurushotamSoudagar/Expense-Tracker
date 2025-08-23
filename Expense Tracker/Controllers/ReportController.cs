8fe07bcc17c3274f9452001685691141c71c50fb
Purushotam Soudagar <purushotam.techstack@gmail.com>
2025-08-23 11:39:06 +0530

    Added Report view page. not yet implemented backend for  that

---
+++ Expense Tracker/Controllers/ReportController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Expense_Tracker.Controllers
+{
+    public class ReportController : Controller
+    {
+        public IActionResult Index()
+        {
+            var budgetData = new List<object>
+            {
+                new { Category = "Food", Budget = 500, Expense = 650 },
+                new { Category = "Rent", Budget = 1000, Expense = 1000 },
+                new { Category = "Travel", Budget = 300, Expense = 250 },
+                new { Category = "Shopping", Budget = 400, Expense = 550 },
+                new { Category = "Bills", Budget = 600, Expense = 620 }
+            };
+
+            ViewBag.BudgetVsExpense = budgetData;
+            return View();
+        }
+    }
+}
