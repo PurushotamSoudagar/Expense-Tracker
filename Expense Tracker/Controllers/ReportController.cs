65e28f32cefa9cad3d92873b082be9d5505d6354
Purushotam Soudagar <purushotam.techstack@gmail.com>
2025-08-24 17:49:04 +0530

    Added logic for Top Categories Expense and Monthly Expense comparison

--- Expense Tracker/Controllers/ReportController.cs
+++ Expense Tracker/Controllers/ReportController.cs
@@ -1,22 +1,82 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Expense_Tracker.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Expense_Tracker.Controllers
 {
     public class ReportController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+        public ReportController(ApplicationDbContext context)
         {
-            var budgetData = new List<object>
-            {
-                new { Category = "Food", Budget = 500, Expense = 650 },
-                new { Category = "Rent", Budget = 1000, Expense = 1000 },
-                new { Category = "Travel", Budget = 300, Expense = 250 },
-                new { Category = "Shopping", Budget = 400, Expense = 550 },
-                new { Category = "Bills", Budget = 600, Expense = 620 }
-            };
+            _context = context;
+        }
+        public async Task<IActionResult> IndexAsync(DateTime? Month)
+        {
+            DateTime defaultMonth = Month ?? DateTime.Now;
+            int selectedYear = defaultMonth.Year;
+            int selectedMonthNumber = defaultMonth.Month;
 
-            ViewBag.BudgetVsExpense = budgetData;
+            // This year This month Transaction Selection
+           
+            var SelectTransactions = await _context.Transactions
+                 .Include(x => x.Category)
+                 .Where(y => y.Date.Year == selectedYear && y.Date.Month == selectedMonthNumber)
+                 .ToListAsync();
+
+            ViewBag.SelectedMonth = defaultMonth.ToString("MMMM yyyy");
+
+            // This year This Month Total Expense
+            var ThisYearMonthExpenses = SelectTransactions
+               .Where(i => i.Category.Type == "Expense")
+               .Sum(y => (decimal?)y.Amount) ?? 0;
+
+           ViewBag.ThisYearMonthExpenses = ThisYearMonthExpenses;
+
+            // Last year This Month Total Expense
+            var SelectLastYearTransactions = await _context.Transactions
+                .Include(x => x.Category)
+                .Where(y => y.Date.Year == selectedYear - 1 && y.Date.Month == selectedMonthNumber)
+                .ToListAsync();
+
+             var LastYearMonthExpenses = SelectLastYearTransactions
+                .Where(i => i.Category.Type == "Expense")
+                .Sum(y => (decimal?)y.Amount) ?? 0;
+
+            ViewBag.LastYearMonthExpenses = LastYearMonthExpenses;
+
+            var expenseComparison = new[]
+{
+    new { Month = defaultMonth.ToString("MMM"), ThisYear = ThisYearMonthExpenses, LastYear = LastYearMonthExpenses }
+};
+
+            ViewBag.ExpenseComparison = expenseComparison;
+
+            //ColChart
+
+            // Bar Chart
+            List<BarChartData> ExpenseSummary = SelectTransactions
+                .Where(i => i.Category.Type == "Expense")
+                .GroupBy(j => j.Category.Title)  // Group by Category
+                .Select(k => new BarChartData
+                {
+                Category = k.Key,           // Category name
+                Amount = k.Sum(j => j.Amount) // Sum of all amounts in that category
+                })
+            .ToList();
+
+            ViewBag.ExpenseSummary = ExpenseSummary;
+
+           
+
+
             return View();
         }
     }
+    public class BarChartData
+    {
+         public string Category { get; set; }
+    public decimal Amount { get; set; }  
+    }
+ 
 }
